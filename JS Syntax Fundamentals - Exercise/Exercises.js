09c18eb5d31b2b0f995135957c26fa7229686a8b
Mustafa Buhov <mustyx900@gmail.com>
2024-05-28 12:13:09 +0300

    Update Exercises.js

--- JS Syntax Fundamentals - Exercise/Exercises.js
+++ JS Syntax Fundamentals - Exercise/Exercises.js
@@ -170,3 +170,79 @@ function checkIfAllNumberDigitsAreSame(number){
 }
 
 checkIfAllNumberDigitsAreSame(1234);
+
+// Write a function that determines whether a driver is within the speed limit.
+// You will receive the speed and the area.
+// Each area has a different limit: 
+
+// •	On the motorway, the limit is 130 km/h
+// •	On the interstate, the limit is 90 km/h
+// •	In the city, the limit is 50 km/h 
+// •	Within a residential area, the limit is 20 km/h
+// If the driver is within the limits, there should be a printed speed and the speed limit. 
+//                 `Driving {speed} km/h in a {speed limit} zone`
+// If the driver is over the limit, however, your function should print the severity of the infraction and the difference in speeds. 
+// `The speed is {difference} km/h faster than the allowed speed of {speed limit} - {status}`
+// For speeding up to 20 km/h over the limit, the status should be speeding.
+// For speeding up to 40 km/h over the limit, the status should be excessive speeding.
+// For anything else, status should be reckless driving.
+
+
+function isDriverWithinTheSpeedLimit(speed, area){
+    const speedLimits = {
+        motorway : 130,
+        interstate : 90,
+        city : 50,
+        residential : 20 
+    }
+
+    let speedLimit = speedLimits[area];
+    
+    if (speed <= speedLimit) {
+        console.log(`Driving ${speed} km/h in a ${speedLimit} zone`);
+    }else{
+        const difference = speed - speedLimit;
+        let status;
+
+        if (difference <= 20) {
+            status = 'speeding';
+        } else if (difference <= 40) {
+            status = 'excessive speeding';
+        } else {
+            status = 'reckless driving';
+        }
+        console.log(`The speed is ${difference} km/h faster than the allowed speed of ${speedLimit} - ${status}`);
+    }
+}
+
+// Write a program that receives 6 parameters which are a number and a list of five operations. Perform the operations sequentially by starting with the input number and using the result of every operation as a starting point for the next one. Print the result of every operation in order. The operations can be one of the following:
+// •	chop - divide the number by two
+// •	dice - square root of a number
+// •	spice - add 1 to the number
+// •	bake - multiply number by 3
+// •	fillet - subtract 20% from the number
+
+
+function cookByNumbers(start, ...operations){
+
+    let number = parseFloat(start);
+
+    const operationsMap = {
+        chop: (num) => num / 2,
+        dice: (num) => Math.sqrt(num),
+        spice: (num) => num + 1,
+        bake: (num) => num * 3,
+        fillet: (num) => num * 0.8
+    };
+
+    for (let operation of operations) {
+
+        if(!operationsMap.hasOwnProperty(operation)){
+            continue;
+        }
+        number = operationsMap[operation](number);
+        console.log(number);
+    }
+}
+
+cookByNumbers('32', 'chop', 'chop', 'mur', 'chop', 'chop','chop');
