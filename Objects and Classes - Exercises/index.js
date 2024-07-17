8a9847558a50c03fb2e2dbe999c79c944d94574b
Mustafa Buhov <mustyx900@gmail.com>
2024-07-17 12:30:20 +0300

    Update index.js

--- Objects and Classes - Exercises/index.js
+++ Objects and Classes - Exercises/index.js
@@ -199,4 +199,166 @@ registerHeroes([
     'Hes / 1 / Desolator, Sentinel, Antara'
     ]
     );
+// 06 Words Tracker
+// Write a function that receives an array of words and
+// finds occurrences of given words in that sentence. 
+// The input will come as an array of strings.
+// The first string will contain the words you
+// will be looking for separated by a space.
+// All strings after that will be the words in which you
+// will check for a match.
+// Print for each word how many times it occurs.
+// The words should be sorted by count in descending
 
+function wordsTracker(array) {
+    let wantedWords = array.shift().split(' ');
+    let wantedWordsCounts = {};
+    wantedWords.forEach(word => wantedWordsCounts[word] = 0);
+    array.forEach(word => word in wantedWordsCounts && wantedWordsCounts[word]++);
+    wantedWords.sort((a, b) => wantedWordsCounts[b] - wantedWordsCounts[a]);
+    wantedWords.forEach(word => console.log(`${word} - ${wantedWordsCounts[word]}`));
+}
+
+// 07 Odd Occurrences
+
+// Write a function that extracts the elements of a sentence,
+// if it appears an odd number of times (case-insensitive).
+// The input comes as a single string. 
+// The words will be separated by a single space.
+
+function oddOccurences(input) {
+    let words = input.split(" ").map(el => el.toLowerCase());
+    let collection = {};
+
+    collection = words.reduce((acc, cur, i) => {
+        if (!acc.hasOwnProperty(cur)) {
+            acc[cur] = 1;
+        } else {
+            acc[cur]++;
+        }
+
+        return acc;
+    }, {});
+
+    collection = Object.entries(collection)
+        .filter(([word, value]) => {
+            if (value % 2 !== 0) {
+                return word;
+            }
+        })
+        .map(el => el[0]);
+
+    console.log(collection.join(" "));
+}
+
+oddOccurences('Java C# Php PHP Java PhP 3 C# 3 1 5 C#');
+
+// 08 Piccolo
+
+//Write a function that:
+// •	Records a car number for every car that enters the parking lot
+// •	Removes a car number when the car goes out
+// •	Input will be an array of strings in format [direction, carNumber]
+// Print the output with all car numbers which are in the parking lot sorted in ascending by number.
+// If the parking lot is empty, print: "Parking Lot is Empty".
+
+function solve(input) {
+    let obj = {};
+
+    obj = input.reduce((acc, cur) => {
+        let [command, carNumber] = cur.split(', ');
+
+        if (acc.hasOwnProperty(carNumber)) {
+            if (command === 'OUT') {
+                acc[carNumber] = 'out';
+            }else if (command === 'IN') {
+                acc[carNumber] = 'parked';
+            }
+        } else {
+            if (command === 'IN') {
+                acc[carNumber] = 'parked';
+            }
+        }
+
+        return acc;
+    }, {});
+
+    obj = Object.entries(obj)
+        .filter(([carNumber, status]) => {
+
+            if (status ===  'parked') {
+                return carNumber;
+            }
+        })
+        .map(el => el[0])
+        .sort((a, b) => a.localeCompare(b))
+
+    if (obj.length > 0) {
+        console.log(obj.join('\n'));
+    } else {
+        console.log('Parking Lot is Empty');
+    }
+}
+
+// 09 Make a Dictionary
+
+// You will receive an array with strings in the form of JSON's. 
+// You have to parse these strings and combine them into one object. Every string from the array will hold terms and a description. If you receive the same term twice, replace it with the new definition.
+// Print every term and definition in that dictionary on new line in format:
+// `Term: ${term} => Definition: ${definition}`
+// Don't forget to sort the dictionary alphabetically by the terms as in real dictionaries.
+
+function dictionary(input){
+    let dict = {};
+    for (let element of input){
+        let obj = JSON.parse(element);
+        dict = Object.assign(dict, obj);        
+    } 
+        
+    let sortedKeys = Object.keys(dict);
+    sortedKeys.sort((a, b) => a.localeCompare(b));   
+     
+    for (let term of sortedKeys) {
+        let definition = dict[term];             
+        console.log(`Term: ${term} => Definition: ${definition}`);
+    }
+}
+
+//10 Class Vehicle
+
+// Create a class with the name Vehicle that has the following properties:
+// •	type – a string
+// •	model – a string
+// •	parts – an object that contains:
+// o	engine – number (quality of the engine)
+// o	power – number
+// o	quality – engine * power
+// •	fuel – a number
+// •	drive – a method that receives fuel loss and decreases the fuel of the vehicle by that number
+// The constructor should receive the type, the model, the parts as an object, and the fuel
+// In judge post your class (Note: all names should be as described)
+
+class Vehicle {
+    constructor(type, model, parts, fuel) {
+        this.type = type;
+        this.model = model;
+        this.parts = {
+            engine: parts.engine,
+            power: parts.power,
+            get quality() {
+                return this.engine * this.power;
+            }
+        };
+        this.fuel = fuel;
+    }
+
+    drive(fuelLoss) {
+        this.fuel -= fuelLoss;
+    }
+}
+
+let parts = { engine: 6, power: 100 };
+let vehicle = new Vehicle('a', 'b', parts, 200);
+vehicle.drive(100);
+console.log(vehicle.fuel);
+console.log(vehicle.parts.quality);
